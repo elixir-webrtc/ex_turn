14df4a546f2e19a85731eef70258c490a71e856d
Michał Śledź <michalsledz34@gmail.com>
2024-04-10 08:58:35 +0200

    Lower Elixir requirement to 1.15

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExTURN.MixProject do
     [
       app: :ex_turn,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
